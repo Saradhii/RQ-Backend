f97db2478480e00d9728a8d1cc8cbd5abb859f84
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-10-17 13:18:01 +0530

    Update FindHSRoute.js

--- routes/FindHSRoute.js
+++ routes/FindHSRoute.js
@@ -2,20 +2,20 @@ const HS = require("../models/HSCodesSchema");
 const Router = require("express");
 const HSRoute = Router();
 
-HSRoute.get("/findhs", async (req, res) => {
+// HSRoute.get("/findhs", async (req, res) => {
 
-    const { name } = req.query;
-    const pr = await HS.find({ $text: { $search: name } });
+//     const { name } = req.query;
+//     const pr = await HS.find({ $text: { $search: name } });
   
-    if (pr <= 0) {
-      res.status(401).send("No Result");
-    }
-     else {
-      res.send(pr);
-    }
-  });
+//     if (pr <= 0) {
+//       res.status(401).send("No Result");
+//     }
+//      else {
+//       res.send(pr);
+//     }
+//   });
 
-HSRoute.get("/findhsa", async (req, res) => {
+HSRoute.get("/findhs", async (req, res) => {
 
   const { search } = req.query;
   const pr = await HS.find({name: { $regex: `${search}`, $options: '$i' }}, {});
