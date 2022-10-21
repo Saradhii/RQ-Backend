ef030047f9b28207d5019ac00e36d3543be876cd
Saradhi <saradhi0001069@gmail.com>
2022-10-21 18:42:34 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://request-quote-intoglo.netlify.app/"],
   })
 );
 app.use("/quote", RequestQoutesRoute);
