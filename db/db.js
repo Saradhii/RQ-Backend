b43dc23562d17a22fffd651154d3630f6446bd43
Saradhi <saradhi0001069@gmail.com>
2022-11-17 11:13:06 +0530

    db url update

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = mongoose.connect(
   // "mongodb+srv://saradhi:saradhi123@cluster0.s0vsou1.mongodb.net/?retryWrites=true&w=majority"
-  "mongodb://127.0.0.1:27017"
+  "mongodb://127.0.0.1:27017/intoglo"
 );
 
 
