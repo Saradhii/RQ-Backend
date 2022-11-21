e2702d8ccd921b374e2fdcc663af1219eca309b5
Saradhi <saradhi0001069@gmail.com>
2022-11-21 18:00:36 +0530

    updated query

--- routes/SearchEngine.js
+++ routes/SearchEngine.js
@@ -15,38 +15,103 @@ const phraseSearch = async (_index, phrase) => {
   const hits = [];
 
   // only string values are searchable
+
   if(phrase.includes(" "))
   {
+    const words = phrase.split(" ");
+    const searchResult = await client.search({
+        index: _index,
+        query:
+        {bool:
+         { must:
+           [ 
+             { multi_match:
+               {
+                query: words[0],
+                type: "phrase_prefix",
+                fields: ["name","description"]
+               }
+            }, 
+            
+            { multi_match: 
+              { 
+               query: words[1],
+               type: "phrase_prefix",
+               fields: ["name","description"]
+              }
+            },
+            
+          ]
+        }
+      }
+      })
+      .catch((e) => console.log("errr", e));
+      return searchResult;
+      }
+     else
+    {
     const searchResult = await client
     .search({
       index: _index,
       query: {
-        multi_match: {
-          query: phrase,
-          fields: ["name","description"],
-          type: "best_fields",
-        },
+        dis_max: {
+          queries: [
+            {
+              multi_match : {
+                query: phrase,
+                type:       "phrase_prefix",
+                fields: ["name","description"],
+                minimum_should_match: "50%" 
+              }
+            },
+            {
+              fuzzy: {
+                description: {
+                  value: phrase,
+                  fuzziness:"AUTO"
+                },
+            }, 
+            }
+          ]
+        }
       },
     })
     .catch((e) => console.log("errr", e));
   return searchResult;
   }
-  else
-  {
-    const searchResult = await client
-    .search({
-      index: _index,
-      query: {
-        multi_match: {
-          query: phrase,
-          fields: ["name","description"],
-          type: "phrase_prefix",
-        },
-      },
-    })
-    .catch((e) => console.log("errr", e));
-  return searchResult;
-  }
+
+  // if(phrase.includes(" "))
+  // {
+  //   const searchResult = await client
+  //   .search({
+  //     index: _index,
+  //     query: {
+  //       multi_match: {
+  //         query: phrase,
+  //         fields: ["name","description"],
+  //         type: "best_fields",
+  //       },
+  //     },
+  //   })
+  //   .catch((e) => console.log("errr", e));
+  // return searchResult;
+  // }
+  // else
+  // {
+  //   const searchResult = await client
+  //   .search({
+  //     index: _index,
+  //     query: {
+  //       multi_match: {
+  //         query: phrase,
+  //         fields: ["name","description"],
+  //         type: "phrase_prefix",
+  //       },
+  //     },
+  //   })
+  //   .catch((e) => console.log("errr", e));
+  // return searchResult;
+  // }
 };
 
 module.exports = {
