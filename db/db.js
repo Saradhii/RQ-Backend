246caf0101874da74079ec45ce80e18630170bc5
Saradhi <saradhi0001069@gmail.com>
2023-01-10 13:58:00 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connection = mongoose.connect(
   // "mongodb+srv://saradhi:saradhi123@cluster0.s0vsou1.mongodb.net/?retryWrites=true&w=majority"
-  "mongodb://127.0.0.1:27017/intoglo"
+  // "mongodb://127.0.0.1:27017/intoglo"
+ "mongodb://intoglo:intoglo@321@35.72.226.153:27017/?authMechanism=DEFAULT&authSource=intoglo"
 );
 
 
